718c879c2e7e186101609847592c4acadd881dc1
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-11-18 03:49:06 +0500

    KeyPairException.php

---
+++ src/Exception/KeyPairException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace ForwardBlock\Protocol\Exception;
+
+/**
+ * Class KeyPairException
+ * @package ForwardBlock\Protocol\Exception
+ */
+class KeyPairException extends ProtocolException
+{
+}

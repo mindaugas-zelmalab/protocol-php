61f9446e1fc7623a1cd9ef0614f64c53ccb65faa
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-11-21 04:31:57 +0500

    abstract transactions

---
+++ src/Transactions/AbstractTx.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace ForwardBlock\Protocol\Transactions;
+
+use Comely\DataTypes\Buffer\Binary;
+use ForwardBlock\Protocol\Protocol;
+
+/**
+ * Class AbstractTx
+ * @package ForwardBlock\Protocol\Transactions
+ */
+abstract class AbstractTx
+{
+    /** @var Protocol */
+    protected Protocol $protocol;
+
+    /** @var int */
+    protected int $version;
+    /** @var int */
+    protected int $flag;
+    /** @var string|null */
+    protected ?string $sender = null;
+    /** @var int */
+    protected int $nonce = 0;
+    /** @var string|null */
+    protected ?string $recipient = null;
+    /** @var string|null */
+    protected ?string $memo = null;
+    /** @var array */
+    protected array $transfers = [];
+    /** @var Binary|null */
+    protected ?Binary $data = null;
+    /** @var array */
+    protected array $signs = [];
+    /** @var int */
+    protected int $fee = 0;
+    /** @var int */
+    protected int $timeStamp;
+    /** @var int */
+    protected int $feePerByte = 0;
+}

5e2c8e7252eb8856c8744f019fff6a79d2668e07
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-11-21 00:35:55 +0500

    checkPublicKey method

--- src/Validator.php
+++ src/Validator.php
@@ -3,12 +3,53 @@ declare(strict_types=1);
 
 namespace ForwardBlock\Protocol;
 
+use Comely\DataTypes\Buffer\Base16;
+use Comely\DataTypes\Buffer\Binary;
+
 /**
  * Class Validator
  * @package ForwardBlock\Protocol
  */
 class Validator
 {
+    /**
+     * @param $pubKey
+     * @param bool|null $compressed
+     */
+    public static function checkPublicKey($pubKey, ?bool $compressed = null): void
+    {
+        $pubKeyStr = null;
+        if ($pubKey instanceof Base16) {
+            $pubKeyStr = $pubKey->binary()->value();
+        } elseif ($pubKey instanceof Binary) {
+            $pubKeyStr = $pubKey->value();
+        } elseif (is_string($pubKey)) {
+            $pubKeyStr = $pubKey;
+        }
+
+        if (!$pubKeyStr) {
+            throw new \InvalidArgumentException(sprintf('Invalid public key arg type, got "%s"', gettype($pubKeyStr)));
+        }
+
+        $prefixes = ["\x02", "\x03", "\x04"];
+        $length = [33, 65];
+        $type = null;
+        if (is_bool($compressed)) {
+            $prefixes = $compressed ? ["\x02", "\x03"] : ["\x04"];
+            $length = $compressed ? [33] : [65];
+            $type = $compressed ? "compressed " : "uncompressed ";
+        }
+
+        $pubKeyLen = strlen($pubKeyStr);
+        if (!in_array($pubKeyLen, $length)) {
+            throw new \LengthException(sprintf('Invalid %spublic key length, got %d bytes', $type, $pubKeyLen));
+        }
+
+        if (!in_array($pubKeyStr[0], $prefixes)) {
+            throw new \DomainException(sprintf('Invalid %spublic key prefix', $type));
+        }
+    }
+
     /**
      * @param $str
      * @param int $size

d513d08df64a0d43987ec996588ae6965f52061a
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-11-22 03:56:14 +0500

    LedgerEntry updates

--- src/Transactions/Receipts/LedgerEntry.php
+++ src/Transactions/Receipts/LedgerEntry.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace ForwardBlock\Protocol\Transactions\Receipts;
 
 use ForwardBlock\Protocol\AbstractProtocolChain;
+use ForwardBlock\Protocol\Math\UInts;
 use ForwardBlock\Protocol\Transactions\AbstractTxReceipt;
 
 /**
@@ -28,11 +29,47 @@ class LedgerEntry
     /** @var bool */
     protected bool $appliedSuccess = false;
 
-    public function __construct(AbstractProtocolChain $p, AbstractTxReceipt $txR)
+    /**
+     * LedgerEntry constructor.
+     * @param AbstractProtocolChain $p
+     * @param AbstractTxReceipt $txR
+     * @param string $hash160
+     * @param int $flag
+     * @param int $amount
+     * @param string|null $asset
+     */
+    public function __construct(AbstractProtocolChain $p, AbstractTxReceipt $txR, string $hash160, int $flag, int $amount, ?string $asset = null)
     {
         $this->p = $p;
         $this->txR = $txR;
 
+        if ($flag < 0 || $flag > 0xff) {
+            throw new \OutOfBoundsException('Invalid TxReceipt.ledgerEntry flag');
+        }
 
+        if ($amount < 0 || $amount > UInts::MAX) {
+            throw new \OutOfRangeException('Invalid TxReceipt.ledgerEntry amount');
+        }
+
+        $this->hash160 = $hash160;
+        $this->flag = $flag;
+        $this->amount = $amount;
+        $this->asset = $asset;
+    }
+
+    /**
+     * @return void
+     */
+    public function markApplied(): void
+    {
+        $this->appliedSuccess = true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isApplied(): bool
+    {
+        return $this->appliedSuccess;
     }
 }

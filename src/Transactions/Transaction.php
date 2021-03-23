f0d5804fe2b4b8bcfd8e4bb9666181bdf8fb8811
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-03-23 16:59:29 +0500

    decodeAs method

--- src/Transactions/Transaction.php
+++ src/Transactions/Transaction.php
@@ -3,10 +3,24 @@ declare(strict_types=1);
 
 namespace ForwardBlock\Protocol\Transactions;
 
+use Comely\DataTypes\Buffer\Binary;
+use ForwardBlock\Protocol\AbstractProtocolChain;
+use ForwardBlock\Protocol\Math\UInts;
+
 /**
  * Class Transaction
  * @package ForwardBlock\Protocol\Transactions
  */
 class Transaction extends AbstractPreparedTx
 {
+    /**
+     * @param AbstractProtocolChain $p
+     * @param Binary $encoded
+     * @return AbstractPreparedTx
+     * @throws \ForwardBlock\Protocol\Exception\TxFlagException
+     */
+    public static function DecodeAs(AbstractProtocolChain $p, Binary $encoded): AbstractPreparedTx
+    {
+        return $p->txFlags()->get(UInts::Decode_UInt2LE($encoded->substr(1, 2)->raw()))->decode($encoded);
+    }
 }

58da9040663ea91c5292a15689fc86377d8ab9ca
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-04-02 10:21:26 +0500

    get public key

--- src/Accounts/ChainAccountInterface.php
+++ src/Accounts/ChainAccountInterface.php
@@ -3,12 +3,19 @@ declare(strict_types=1);
 
 namespace ForwardBlock\Protocol\Accounts;
 
+use ForwardBlock\Protocol\KeyPair\PublicKey;
+
 /**
  * Interface ChainAccountInterface
  * @package ForwardBlock\Protocol\Accounts
  */
 interface ChainAccountInterface
 {
+    /**
+     * @return PublicKey
+     */
+    public function getPublicKey(): PublicKey;
+
     /**
      * Must return hash160 in BASE16!
      * @return string

36ad3cbfde39002d514c085852f02f459e35dc38
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-04-02 10:21:42 +0500

    recipient trait

--- src/Transactions/Traits/RecipientTrait.php
+++ src/Transactions/Traits/RecipientTrait.php
@@ -3,11 +3,24 @@ declare(strict_types=1);
 
 namespace ForwardBlock\Protocol\Transactions\Traits;
 
+use ForwardBlock\Protocol\Accounts\ChainAccountInterface;
+
 /**
  * Trait RecipientTrait
  * @package ForwardBlock\Protocol\Transactions\Traits
  */
 trait RecipientTrait
 {
+    /**
+     * @param ChainAccountInterface $recipient
+     * @return $this
+     */
+    public function sendToRecipient(ChainAccountInterface $recipient): self
+    {
+        if (isset($this->recipientPubKey)) {
+            $this->recipientPubKey = $recipient->getPublicKey();
+        }
 
+        return $this;
+    }
 }
